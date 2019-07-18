17a06faa6a9c5d6b5601821f74577c97faccd2df
Claire Ellul <ucfscde@ucl.ac.uk>
2019-07-18 10:31:01 -0400

    reference the utilites.js

---
+++ utilites.js
@@ -0,0 +1,38 @@
+var httpPortNumber;
+
+var httpsPortNumber;
+
+function getPort(){
+	alert("seeking port")
+
+	var portXhr = new XMLHttpRequest();
+
+	portXhr.addEventListener("load", function () {
+
+		var parser = new DOMParser();
+
+		var doc = parser.parseFromString(portXhr.responseText, "application/xml");
+
+		httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
+
+		httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+
+		console.log("Port : " + httpPortNumber);
+
+		});
+
+
+
+	// depending on whether we are in a browser or on a phone
+
+	// the location of the config file is different
+
+	// if we are on a phone then http and https won't be present
+
+	var configLocation = "res/port.xml";
+
+	portXhr.open("get", configLocation, true);
+
+	portXhr.send();
+
+}
